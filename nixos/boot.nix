5fcf93e3de6bb9f86b964a905d6e48605bf1b89c
VinoBandit <29282675+6FaNcY9@users.noreply.github.com>
2026-05-16 03:55:23 +0200

    Refactor boot configuration in boot.nix

--- nixos/boot.nix
+++ nixos/boot.nix
@@ -1,7 +1,17 @@
 { ... }:
 {
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    loader = {
+      systemd-boot = {
+        enable = true;
+        configurationLimit = 10;
+        consoleMode = "auto";
+      };
+      efi.canTouchEfiVariables = true;
+    };
+    kernelPackages = pkgs.linuxPackages_latest;
+    tmp.useTmpfs = true;
+  };
 
   security.rtkit.enable = true;
 }
