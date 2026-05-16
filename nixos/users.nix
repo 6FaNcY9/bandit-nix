159fa6b8cabe382e359f444efaec6cb5cff5eed1
VinoBandit <29282675+6FaNcY9@users.noreply.github.com>
2026-05-16 05:35:25 +0200

    Require password for sudo access in wheel group
    Set wheel group to require password for sudo access.

--- nixos/users.nix
+++ nixos/users.nix
@@ -12,6 +12,9 @@
       "input"
       "storage"
     ];
+
+    security.sudo.wheelNeedsPassword = true;  # default but explicit is better
+  
     shell = pkgs.fish;
     # temporary until sops is configured — replace with hashedPasswordFile
     hashedPassword = "$6$w/id8WONcOVFgaLH$Y92T1W3/n6pNy5bXYv7M8pyAqa6f1kpskszGXort4pjy3nDJW3ZN.1cdZpHwHab3huKNWNWLWPw9ZxkaAO4fK"; # run: mkpasswd -m sha-512
