68ddfda27e1b700c0c4e0ad0fb3c1a1f47ba8437
VinoBandit <29282675+6FaNcY9@users.noreply.github.com>
2026-05-16 03:49:26 +0200

    Enhance Nix settings with auto-optimise and GC
    Refactor Nix settings to include auto-optimisation and garbage collection configuration.

--- nixos/core.nix
+++ nixos/core.nix
@@ -3,7 +3,19 @@
   i18n.defaultLocale = "en_US.UTF-8";
   time.timeZone = "Europe/Vienna";
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix = {
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      auto-optimise-store = true;
+      allowed-users = [ "vino" ];
+      trusted-users = [ "root" "vino" ];
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+  };
 
   fonts.fontconfig.enable = true;
 }
