80e4fddb1f5579ab9ec59cf783584a751efc7961
VinoBandit <29282675+6FaNcY9@users.noreply.github.com>
2026-05-16 04:02:49 +0200

    Refactor fish program configuration in shell.nix

--- home/shell.nix
+++ home/shell.nix
@@ -1,4 +1,25 @@
 { ... }:
 {
-  programs.fish.enable = true;
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      set fish_greeting ""
+    '';
+    shellAbbrs = {
+      # nix
+      ns  = "nixos-rebuild switch --flake .#bandit";
+      nt  = "nixos-rebuild test --flake .#bandit";
+      nfu = "nix flake update";
+      ngc = "nix-collect-garbage -d";
+      # navigation
+      ll  = "eza -la";
+      cat = "bat";
+      cd  = "z";
+    };
+  };
+
+  programs.eza.enable    = true;
+  programs.bat.enable    = true;
+  programs.zoxide.enable = true;
+  programs.fzf.enable    = true;
 }
