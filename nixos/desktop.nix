dc7f2e7015ef3f6833fb994ab4aa20a5d873bbdb
6FaNcY9 <29282675+6FaNcY9@users.noreply.github.com>
2026-07-31 17:14:20 +0200

    fix(desktop): launch tuigreet session via start-hyprland

--- nixos/desktop.nix
+++ nixos/desktop.nix
@@ -3,7 +3,7 @@
     greetd = {
       enable = true;
       useTextGreeter = true;
-      settings.default_session.command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd Hyprland";
+      settings.default_session.command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd start-hyprland";
     };
 
     # For mounting/unmounting drives in file managers.
